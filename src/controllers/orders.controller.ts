6f087a5c9a232aad6313a0f2e505275f933558cd
cathe <cacateles12@gmail.com>
2022-08-01 19:43:57 -0300

    Added function list to orders controller

---
+++ src/controllers/orders.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+import listAllOrders from '../services/orders.service';
+
+const list = async (_req: Request, res: Response) => {
+  const result = await listAllOrders();
+  return res.status(201).json(result);
+};
+
+export default list;
