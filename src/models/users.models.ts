b3be210d9c1270608becc427037b9a23c52429b3
cathe <cacateles12@gmail.com>
2022-08-01 15:04:06 -0300

    Added users model

---
+++ src/models/users.models.ts
@@ -0,0 +1,19 @@
+import { ResultSetHeader } from 'mysql2';
+import User from '../interfaces/users.interfaces';
+import connection from './connection';
+
+const addUser = async (user: User) => {
+  const { username, classe, level, password } = user;
+  const result = await connection.execute<ResultSetHeader>(
+    `INSERT INTO
+    Trybesmith.Users(username, classe, level, password)
+  VALUES
+    (?, ?, ?, ?);`,
+    [username, classe, level, password],
+  );
+  const [dataInserted] = result;
+  const { insertId } = dataInserted;
+  return { id: insertId, ...user };
+};
+
+export default addUser;
